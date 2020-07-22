04845a55a8ee38509561c14041beba7bba7a67dc
Rohit Pardeshi <rohitpardeshi04@users.noreply.github.com>
2020-07-22 14:36:39 +0530

    gallery fixes

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,16 +1,20 @@
 let data;
 
-function onJsonLoad(res){
-	data = res;
-	data.itemsBaseURL = `${window.location.origin}/assets/images/gallery/`;
-	console.log(data);
-	$("#nanogallery2").nanogallery2(data);
+function onJsonLoad(res) {
+  data = res;
+  data.itemsBaseURL = `${window.location.origin}/assets/images/gallery/`;
+  data.thumbnailHeight = 250;
+  data.thumbnailWidth = 400;
+  data.thumbnailGutterWidth = 200;
+  data.thumbnailGutterHeight = 50;
+  console.log(data);
+  $("#nanogallery2").nanogallery2(data);
 }
 
-$(document).ready(function(){
-	$.ajax({
-		url: "/assets/images/gallery/list.json",
-		method: "get",
-		success: onJsonLoad
-	});
+$(document).ready(function () {
+  $.ajax({
+    url: "/assets/images/gallery/list.json",
+    method: "get",
+    success: onJsonLoad,
+  });
 });
